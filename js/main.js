9a170aabe875e8b17d6c8d967784a785c69b6efb
kylereese2019 <56153392+kylereese2019@users.noreply.github.com>
2019-10-11 15:18:34 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -5,3 +5,21 @@ window.onload = () => {
     navigator.serviceWorker.register('./sw.js');
   }
 }
+
+window.addEventListener('online', function(e) {
+    // Resync data with server.
+    console.log("You are online");
+}, false);
+
+window.addEventListener('offline', function(e) {
+    // Queue up events for server.
+    console.log("You are offline");
+}, false);
+
+// Check if the user is connected.
+if (navigator.onLine) {
+    console.log("Connected");
+} else {
+    // Show offline message
+    console.log("No Internet access");
+}
