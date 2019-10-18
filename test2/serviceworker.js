f07ed9842f29c4130f4808eee04526fc18127040
kylereese2019 <56153392+kylereese2019@users.noreply.github.com>
2019-10-18 15:53:00 +0200

    cache

--- test2/serviceworker.js
+++ test2/serviceworker.js
@@ -1,7 +1,15 @@
+self.addEventListener("install", function(event) {
+  event.waitUntil(
+    caches.open("test2-cache").then(function(cache) {
+      return cache.add("/test2/test2-offline.html");
+    })
+  );
+});
+
+/*
 self.addEventListener("fetch", function(event) {
   console.log("Fetch request for: ", event.request.url);
 
-  /*
   if (event.request.url.includes("style2.css")) {
     event.respondWith(
       new Response(
@@ -10,7 +18,6 @@ self.addEventListener("fetch", function(event) {
       )
     )
   };
-  */
   
   event.respondWith(
     fetch(event.request).catch(function() {
@@ -18,3 +25,4 @@ self.addEventListener("fetch", function(event) {
     })
   );
 });
+*/
