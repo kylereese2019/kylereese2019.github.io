d96240cf0a7be62a2e82dce531008f674f123b3b
kylereese2019 <56153392+kylereese2019@users.noreply.github.com>
2019-10-18 14:50:38 +0200

    Update serviceworker.js

--- test2/serviceworker.js
+++ test2/serviceworker.js
@@ -2,6 +2,7 @@ self.addEventListener("fetch", function(event) {
   console.log("Fetch request for: ", event.request.url);
 
   /*
+  */
   if (event.request.url.includes("style2.css")) {
     event.respondWith(
       new Response(
@@ -9,7 +10,6 @@ self.addEventListener("fetch", function(event) {
         { headers: { "Content-Type": "text/css" }}
       )
     );
-  */
   
   event.respondWith(
     fetch(event.request).catch(function() {
