731a8ba889d67348a6b1e74a2ac545109bda1899
kylereese2019 <56153392+kylereese2019@users.noreply.github.com>
2019-10-18 14:48:57 +0200

    Update serviceworker.js

--- test2/serviceworker.js
+++ test2/serviceworker.js
@@ -1,6 +1,16 @@
 self.addEventListener("fetch", function(event) {
   console.log("Fetch request for: ", event.request.url);
 
+  /*
+  if (event.request.url.includes("style2.css")) {
+    event.respondWith(
+      new Response(
+        ".title {background: red!important;}",
+        { headers: { "Content-Type": "text/css" }}
+      )
+    );
+  */
+  
   event.respondWith(
     fetch(event.request).catch(function() {
       return new Response("You're offline.");
