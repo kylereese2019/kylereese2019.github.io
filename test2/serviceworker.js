a00dfb05e24efa7c1daa76976670359890921d28
kylereese2019 <56153392+kylereese2019@users.noreply.github.com>
2019-10-25 14:51:26 +0200

    chap 4 - 2

--- test2/serviceworker.js
+++ test2/serviceworker.js
@@ -13,7 +13,7 @@ self.addEventListener("fetch", function(event) {
     console.log("Fetch request for:", event.request.url);
     event.respondWith(
       new Response(
-        ".title {background: red!important;}",
+        ".title {background: yellow!important;}",
         { headers: { "Content-Type": "text/css" }}
       )
     );
